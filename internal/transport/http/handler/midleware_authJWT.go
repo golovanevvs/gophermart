f29ae3c7e32d53328b0f81f2d2babc086ef04f01
golovanevvs <lony@inbox.ru>
2024-11-04 19:28:39 +0300

    Debug auth

--- internal/transport/http/handler/midleware_authJWT.go
+++ internal/transport/http/handler/midleware_authJWT.go
@@ -25,12 +25,13 @@ func (hd *handlerStr) authByJWT(next http.Handler) http.Handler {
 		}
 
 		headerSplit := strings.Split(header, " ")
+		fmt.Println(header)
 		fmt.Println(headerSplit)
 
-		// if len(headerSplit) != 2 || headerSplit[0] != "Bearer" {
-		// 	http.Error(w, "некорректный заголовок авторизации", http.StatusUnauthorized)
-		// 	return
-		// }
+		if len(headerSplit) != 2 || headerSplit[0] != "Bearer" {
+			http.Error(w, "некорректный заголовок авторизации", http.StatusUnauthorized)
+			return
+		}
 
 		if len(headerSplit[1]) == 0 {
 			http.Error(w, "заголовок авторизации не содержит токен", http.StatusUnauthorized)
